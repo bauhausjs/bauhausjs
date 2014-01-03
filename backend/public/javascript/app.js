71a2567950415640ab6325d7f2c123b856eaf174
Philipp Schächtele <mail@philj.de>
2014-01-03 17:49:13 +0100

    Add angular dependencies

--- backend/public/javascript/app.js
+++ backend/public/javascript/app.js
@@ -1,4 +1,4 @@
-angular.module('bauhaus', ['ngResource', 'ngRoute', 'bauhaus.dashboard'])
+angular.module('bauhaus', ['ngResource', 'ngRoute', 'bauhaus.dashboard', 'bauhaus.page'])
     .config(function ($routeProvider) {
         'use strict';
         $routeProvider.when('/', {redirectTo: '/dashboard'});
