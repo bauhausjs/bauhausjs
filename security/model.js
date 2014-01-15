160a7e4a11db747ea6fcbba7796c4c011c4aa799
Philipp Schächtele <mail@philj.de>
2014-01-15 09:45:28 +0100

    Change password API

--- security/model.js
+++ security/model.js
@@ -12,9 +12,28 @@ user.schema.plugin(passportLocalMongoose);
 
 user.model = mongoose.model('User', user.schema);
 
-baucis.rest({
+var controller = baucis.rest({
     singular:'User', 
     select:'username password', swagger: true
+});
+
+/* Middleware which is add for put method (=update user) to store password after user was stored */
+controller.documents('put', function (req, res, next) {
+    if (req.body && req.body.password && req.body.password.length > 0) {
+        user.model.findOne({_id: req.params.id }, function (err, user) {
+            if (user._id) {
+                user.setPassword(req.body.password, function () {
+                    user.save(function () {
+                        next();
+                    });
+                });
+            } else {
+                next();
+            }
+        });
+    } else {
+        next();
+    }
 });
 
 
