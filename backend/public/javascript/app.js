ac2494854d63254d1fca6baeabad40f71827e08a
Philipp Schächtele <mail@philj.de>
2014-01-15 09:48:01 +0100

    Load user module

--- backend/public/javascript/app.js
+++ backend/public/javascript/app.js
@@ -1,4 +1,4 @@
-angular.module('bauhaus', ['ngResource', 'ngRoute', 'bauhaus.dashboard', 'bauhaus.page'])
+angular.module('bauhaus', ['ngResource', 'ngRoute', 'bauhaus.dashboard', 'bauhaus.page', 'bauhaus.user'])
     .config(function ($routeProvider) {
         'use strict';
         $routeProvider.when('/', {redirectTo: '/dashboard'});
