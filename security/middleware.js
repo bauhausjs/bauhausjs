67bfeaeb94e2724341369b963281cdc7437d2501
Philipp Schächtele <mail@philj.de>
2014-02-25 18:22:21 +0100

    Remove mongoose dependency from security middleware, add addUserToRender middleware

--- security/middleware.js
+++ security/middleware.js
@@ -1,48 +1,62 @@
-var debug = require('debug')('bauhaus:security')
+var debug = require('debug')('bauhaus:security'),
+    Role = require('./model/role');
 
 var middleware = module.exports = {};
 
-middleware.loadRoles = function (mongoose) {
-    return function loadRoles (req, res, next) {
-        // User roles and permissions are added as soon user is authorized by passport
-        // Info is persisted for the hole session, user must login again to receive new roles and permissions
-        if (req.user && !req.session.user) {
-            mongoose.models.Role.find({'_id': { $in: req.user.roles }}, function (err, docs) {
-                if (err) next();
+middleware.loadUser = function loadUser (req, res, next) {
+    // User roles and permissions are added as soon user is authorized by passport
+    // Info is persisted for the hole session, user must login again to receive new roles and permissions
+    if (req.user && !req.session.user) {
+        Role.find({'_id': { $in: req.user.roles }}, function (err, docs) {
+            if (err) next();
 
-                var roles = [];
-                var permissions = [];
-                for (var d in docs) {
-                    var role = docs[d];
-                    roles.push(role.name);
-                    if (role.permissions) {
-                        for (var permission in role.permissions) {
-                            if (permissions.indexOf(permission) !== 0) {
-                                permissions.push(permission);
-                            }
+            var roles = [];
+            var permissions = [];
+            for (var d in docs) {
+                var role = docs[d];
+                roles.push(role.name);
+                if (role.permissions) {
+                    for (var permission in role.permissions) {
+                        if (permissions.indexOf(permission) !== 0) {
+                            permissions.push(permission);
                         }
                     }
                 }
-
-                var user = {
-                    id: req.user._id,
-                    username: req.user.username,
-                    roles: roles,
-                    permissions: permissions
-                }
-                // add user info to session
-                req.session.user = user;
-
-                next();
-            })
-        } else {
-            if (!req.user) {
-                // remove user info from session
-                req.session.user = null;
             }
+
+            var user = {
+                id: req.user._id,
+                username: req.user.username,
+                roles: roles,
+                permissions: permissions
+            }
+            // add user info to session
+            req.session.user = user;
+
             next();
+        })
+    } else {
+        if (!req.user) {
+            // remove user info from session
+            req.session.user = null;
+        }
+        next();
+    }
+
+};
+
+
+middleware.addUserToRender = function addUserToRender (req, res, next) {
+    if (req.bauhaus) {
+
+        req.bauhaus.user = req.session.user;
+        
+        if (req.flash) {
+            var flash = req.flash();
+            req.bauhaus.authError = flash.error;
         }
     }
+    next();
 };
 
 /**
