96af6080cc6348b0f8577a26b35cea81291a3816
Philipp Schächtele <mail@philj.de>
2014-03-04 17:26:49 +0100

    Replace populated objects by mongo ids for documents

--- document/client/document/documentController.js
+++ document/client/document/documentController.js
@@ -35,36 +35,68 @@ angular.module('bauhaus.document.controllers').controller('DocumentDetailCtrl', 
 
     $scope.service = DocumentService($scope.type);
 
-    $scope.customFields = {};
+    $scope.modelConfig = {};
 
     $scope.$watch('documentInfos.all.' + $scope.type, function (newVal) {
         if (newVal && newVal.fields) {
-            $scope.customFields = newVal;
+            $scope.modelConfig = newVal;
+        }
+
+        if ($routeParams.id && $routeParams.id != 'new') {
+            var query = $scope.modelConfig.query ? angular.copy($scope.modelConfig.query) : {};
+            query.id = $routeParams.id;
+            // load document data for passed id
+            $scope.service.get(query, function (result) {
+                if (result && result._id) {
+                    $scope.document = result;
+                }
+            });
+        } else {
+            $scope.document = {};
         }
     });
-
+    // reference shared documentInfo service to init loading
     $scope.documentInfos = SharedDocuments.store;
 
-    if ($routeParams.id && $routeParams.id != 'new') {
-        $scope.documentId = $routeParams.id;
-        // load document data for passed id
-        $scope.service.get({ id: $scope.documentId }, function (result) {
-            if (result && result._id) {
-                $scope.document = result;
-            }
-        });
-    } else {
-        $scope.document = {};
-    } 
 
     $scope.isNew = function () {
         return ($scope.document && $scope.document._id) ? false : true;
     };
 
+    /* Replaces all objects with a field _id by a string of the id */
+    function unpopulate (object) {
+        var obj = angular.copy(object);
+        for (var f in obj) {
+            if (obj.hasOwnProperty(f)) {
+                var field = obj[f];
+                // replace field with object by id
+                if (typeof field === 'object' && field._id) {
+                    obj[f] = field._id;
+                }
+                // replace field with array of objects by array of ids
+                if (Array.isArray(field) && 
+                    field.length > 0 && 
+                    typeof field[0] === 'object' &&
+                    field[0]._id) {
+
+                    var ids = [];
+                    for (var o in field) {
+                        if (field[o]._id) {
+                            ids.push(field[o]._id);
+                        }
+                    }
+                    obj[f] = ids;
+                }
+            }
+        }
+        return obj;
+    }
+
     $scope.updateDocument = function () {
+        var doc = unpopulate($scope.document);
         // Save document if it already has an _id
         if ($scope.document._id) {
-            $scope.service.put($scope.document, function (result) {
+            $scope.service.put(doc, function (result) {
 
             });
         } else {
@@ -73,7 +105,7 @@ angular.module('bauhaus.document.controllers').controller('DocumentDetailCtrl', 
                 $scope.document._id = result._id;
                 $scope.documentId =   result._id;
 
-                $scope.service.put($scope.document, function (result) {
+                $scope.service.put(doc, function (result) {
                     // document saved
                 });
             })
