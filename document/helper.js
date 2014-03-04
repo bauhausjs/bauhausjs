987977720b4959a5797deaee3c0077443eb3780a
Philipp Schächtele <mail@philj.de>
2014-03-04 17:21:36 +0100

    Add helper to parse population paths out of form config

---
+++ document/helper.js
@@ -0,0 +1,22 @@
+
+var helper = module.exports = {};
+
+helper.populateConfig = function (config, pathPrefix) {
+    pathPrefix = pathPrefix || '';
+
+    var populate = [];
+
+    var fields = config.fields;
+    for (var f in fields) {
+        var field = fields[f];
+        if (field.type === 'relation' && field.options && field.options.model) {
+            var model = field.options.model;
+            populate.push({
+                path: pathPrefix + field.name,
+                model: model
+            });
+        }
+    }
+
+    return populate;
+};
