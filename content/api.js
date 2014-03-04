fc9f8bb0e104958856ba079872f4d953f5c0a474
Philipp Schächtele <mail@philj.de>
2014-03-04 17:24:43 +0100

    Populate related docs in content api

--- content/api.js
+++ content/api.js
@@ -1,6 +1,7 @@
 var baucis = require('baucis'),
     async = require('async'),
-    Content = require('./model/content');
+    Content = require('./model/content')
+    populateConfig = require('../document/helper').populateConfig;
 
 module.exports = function (bauhausConfig) {
     var controller = baucis.rest({
@@ -14,12 +15,20 @@ module.exports = function (bauhausConfig) {
             // If result contains array result, perform population on all documents
             var populateParallel = [];
             for (var d in req.baucis.documents) {
+
+                var contentType = bauhausConfig.contentTypes[ req.baucis.documents[d]._type ];
+                var populationConfig = populateConfig(contentType, 'content.');
+
                 // Add callback in Closure
-                (function (doc) {
+                (function (doc, config) {
                     populateParallel.push(function (callback) {
-                        doc.populateFields('content', callback);
+                        if (config.length > 0) {
+                            doc.populate(config, callback);
+                        } else {
+                            callback(null, doc);
+                        }
                     });
-                })(req.baucis.documents[d]);
+                })(req.baucis.documents[d], populationConfig);
             }
             // Perform parallel population on all documents (documents are checked for references,
             // if there are any, there are populated)
@@ -29,11 +38,18 @@ module.exports = function (bauhausConfig) {
                 req.baucis.documents = result;
                 next();
             });
-        } else if (typeof req.baucis.documents === 'object' && req.baucis.documents.populateFields) {
-            // If result contains object, populate it 
-            req.baucis.documents.populateFields('content', function (err, result) {
-                next(err);
-            });
+        } else if (typeof req.baucis.documents === 'object' && req.baucis.documents._type) {
+            var contentType = bauhausConfig.contentTypes[ req.baucis.documents._type ];
+            var populationConfig = populateConfig(contentType, 'content.');
+            if (populationConfig.length > 0) {
+                // If result contains object, populate it 
+                req.baucis.documents.populate(populationConfig, function (err, result) {
+                    next(err);
+                });
+            } else {
+                // Return document directly if there is nothing to populate
+                next();
+            }
         } else {
             return next();
         }
