0e3bd7dd6b9b5539d371a824ba410c2b1079610c
Philipp Schächtele <mail@philj.de>
2014-04-22 10:25:50 +0200

    Fix rendering of dynamic content

--- content/middleware.js
+++ content/middleware.js
@@ -81,7 +81,7 @@ middleware.renderContent = function (contentTypes) {
             if (typeName in contentTypes) {
                 var contentType = contentTypes[typeName];
                 if (typeof contentType.render === 'function') {
-                    (function (data, render) {
+                    (function (data, contentType) {
                         renderParallel.push(function (callback) {
                             contentType.render(req, res, data, contentType.template, function (err, html) {
                                 if (err) debug("Error when calling custom render method of content " + data._id + " of type " + typeName, err);
