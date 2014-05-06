65a79108d258f9e949aa6bfd55f5ffcfc62214aa
Philipp Schächtele <mail@philj.de>
2014-05-06 12:59:55 +0200

    Update page test

--- page/test/middlewareTest.js
+++ page/test/middlewareTest.js
@@ -16,7 +16,7 @@ describe('PageMiddleware', function () {
             });
             // create new page in DB
             page.save(function () { 
-                var req = { url: path };
+                var req = { path: path };
                 var res = {};
                 var next = function () {
                     assert(req.bauhaus, "Defined bauhaus obj in req");
